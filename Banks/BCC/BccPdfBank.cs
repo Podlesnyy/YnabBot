480f085ae5ad9ac517d378995ebc61e1c5b054a6
Podlesnyy Andrey <podlesniy@gmail.com>
2025-01-10 14:20:55 +0300

    Update BccPdfBank.cs

--- Banks/BCC/BccPdfBank.cs
+++ Banks/BCC/BccPdfBank.cs
@@ -3,26 +3,25 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Text;
 using System.Text.RegularExpressions;
 using Adp.Banks.Interfaces;
 using Aspose.Pdf;
 using Aspose.Pdf.Text;
-using CsvHelper;
-using CsvHelper.Configuration;
 
 namespace Adp.Banks.BCC;
 
 public sealed class BccPdfBank : IBank
 {
-    private static readonly CultureInfo RussianCi = new("ru");
     private string bankAccount;
 
-    public bool IsItYour( string fileName ) => fileName.Contains( "pkg_w_mb_main" );
+    public bool IsItYour(string fileName)
+    {
+        return fileName.Contains("pkg_w_mb_main");
+    }
 
     public string FileEncoding => "utf-8";
 
-    public List< Transaction > Parse( MemoryStream fileContent )
+    public List<Transaction> Parse(MemoryStream fileContent)
     {
         var extractFromPdfFile = ExtractFromPdfFile(fileContent);
         GetBankAccount(extractFromPdfFile);
@@ -30,53 +29,63 @@ public sealed class BccPdfBank : IBank
 
         return transactions;
     }
-    
+
     private List<Transaction> GetTransactions(List<string> pdfTextLines)
     {
         var ret = new List<Transaction>();
-        
-        const string pattern = @"(?<date>\d{2}\.\d{2}\.\d{4}(?: \d{2}:\d{2}:\d{2})?)\s*(?<text>.+?)\s+(?<summa>-?\d[\d\s]*,\d{2})";
-        //string pattern = @"(?<date>\d{2}\.\d{2}\.\d{4}(?: \d{2}:\d{2}:\d{2})?)\s*(?<text>.+?)\s+((?:-?\d[\d\s]*,\d{2}\s*)+)";
+
+        const string patternKzt =
+            @"(?<date>\d{2}\.\d{2}\.\d{4})\s+\d{2}:\d{2}:\d{2}\s+\d{2}\.\d{2}\.\d{4}\s+(?<text>.+?)\s+-?\d[\d\s.,]*\s+KZT\s+(?<number>-?\d[\d\s]+,\d\d)\s";
+        const string patternOther =
+            @"(?<date>\d{2}\.\d{2}\.\d{4})\s+(?<description>.+?)\s+(?<amount>-?\d+,\d+)\s+\d+,\d+";
 
         foreach (var line in pdfTextLines)
         {
-            var match = Regex.Match(line, pattern);
+            var match = Regex.Match(line, patternKzt);
 
-            if (!match.Success) continue;
-            
-            var dateParsed = DateTime.TryParseExact(
-                match.Groups["date"].Value,
-                ["dd.MM.yyyy HH:mm:ss", "dd.MM.yyyy"],
-                CultureInfo.InvariantCulture,
-                DateTimeStyles.None,
-                out var date
-            );
-
-            if (!dateParsed)
+            if (match.Success)
             {
-                Console.WriteLine($"Ошибка парсинга даты для строки: {line}");
+                ret.Add(GetFromKzt(match));
                 continue;
             }
 
-            // Получаем текст
-            var text = match.Groups["text"].Value.Trim();
-            
-            
-            // Парсим сумму
-            var summaString = match.Groups["summa"].Value.Replace(" ", ""); // Убираем пробелы в числе
-            var summa = -1 * double.Parse(summaString, new CultureInfo("ru-RU")); // Парсинг с учетом формата
+            match = Regex.Match(line, patternOther);
 
-            ret.Add( new Transaction(bankAccount, date, summa, text, 0, null, null));
+            if (!match.Success) continue;
+            ret.Add(GetFromAnother(match));
         }
+
         return ret;
     }
-    
+
+    private Transaction GetFromAnother(Match match)
+    {
+        var date = DateTime.ParseExact(match.Groups["date"].Value, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+        var description = match.Groups["description"].Value.Trim();
+        var amount = double.Parse(match.Groups["amount"].Value.Replace(',', '.'), CultureInfo.InvariantCulture);
+
+        return new Transaction(bankAccount, date, amount, description, 0, null, null);
+    }
+
+    private Transaction GetFromKzt(Match match)
+    {
+        var date = DateTime.ParseExact(match.Groups["date"].Value, "dd.MM.yyyy", CultureInfo.InvariantCulture);
+
+        // Получаем текст
+        var text = match.Groups["text"].Value.Trim();
+        // Получаем число и заменяем запятую на точку для преобразования
+        var summa = -1 * double.Parse(match.Groups["number"].Value.Replace(" ", "").Replace(",", "."),
+            CultureInfo.InvariantCulture);
+
+        return new Transaction(bankAccount, date, summa, text, 0, null, null);
+    }
+
     private void GetBankAccount(List<string> extractFromPdfFile)
     {
         bankAccount = extractFromPdfFile[1].Trim();
     }
 
-   private static List< string > ExtractFromPdfFile( Stream fileContent )
+    private static List<string> ExtractFromPdfFile(Stream fileContent)
     {
         var pdfDocument = new Document(fileContent);
         var textAbsorber = new TextAbsorber();
