9d959c48442eb546c718776527d53245e77005a0
Podlesnyy Andrey <podlesniy@gmail.com>
2025-02-03 09:02:09 +0900

    nugetss

--- BankTest/Program.cs
+++ BankTest/Program.cs
@@ -6,11 +6,11 @@ using Adp.Banks.SberBank;
 
 Encoding.RegisterProvider( CodePagesEncodingProvider.Instance );
 
-var bank = new BccPdfBank();
+var bank = new OzonBank.OzonBank();
 //_ = bank.Parse( File.ReadAllText( @"f:\operations Sat Aug 03 07_13_52 MSK 2024-Sun Aug 18 08_33_50 MSK 2024.ofx", Encoding.GetEncoding( bank.FileEncoding ) ) );
 // ReSharper disable once UnusedVariable
-var trans = bank.Parse( new MemoryStream( File.ReadAllBytes("/Users/andr/Downloads/!pkg_w_mb_main (4).pdf") ) );
+//var trans = bank.Parse( new MemoryStream( File.ReadAllBytes("/Users/andr/Downloads/!pkg_w_mb_main (4).pdf") ) );
 //var trans = bank.Parse( new MemoryStream( File.ReadAllBytes("/Users/andr/Downloads/!pkg_w_mb_main.pdf") ) );
-
+var trans = bank.Parse(new MemoryStream(File.ReadAllBytes("/Users/andr/Downloads/ozonbank_document_9022954 (1).pdf")));
 
 Console.ReadLine();
