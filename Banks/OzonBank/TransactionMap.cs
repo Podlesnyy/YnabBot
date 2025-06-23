4ccd52f4c3af7e9f96aa76d6aa966bcff9aafc74
Podlesnyy Andrey <podlesniy@gmail.com>
2025-06-23 10:50:31 +0300

    1

--- Banks/OzonBank/TransactionMap.cs
+++ Banks/OzonBank/TransactionMap.cs
@@ -9,14 +9,14 @@ internal sealed class TransactionMap : ClassMap< Transaction >
 {
     public TransactionMap()
     {
-        Map( static m => m.Date ).Name( "Дата операции" ).TypeConverterOption.Format( "dd.MM.yyyy HH:mm:ss" );
+        Map( static m => m.Date ).Name( "Дата операции" ).TypeConverterOption.Format("dd.MM.yyyy HH:mm:ss");
         Map( static m => m.Id ).Name( "Документ" );
         Map( static m => m.Memo ).Name( "Назначение платежа" );
         Map( static m => m.Amount ).
             Name( "Сумма операции" ).
             Convert( static args =>
                      {
-                         var value = args.Row.GetField( "Сумма операции" )!.Replace( " ", "" ).Replace( "+", "" );
+                         var value = args.Row.GetField( "Сумма операции" )!.Replace( " ", "" ).Replace( "+", "" ).Replace("₽","");
                          return double.Parse( value, CultureInfo.InvariantCulture ) * -1;
                      } );
 
